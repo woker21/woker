f61e515afee492584097553b5c2f5c2c12233a8b
Woker21 <112359676+woker21@users.noreply.github.com>
2024-02-08 18:43:26 +0100

    head

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 
+
+
 export default function Home() {
   return (
   <div>
-    <h1>Hola people, Woker Here</h1>
+    
+    <h1 className="woker">Woker Here.</h1>
+    
   </div>
     );
 }
