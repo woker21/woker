65ce052de579e89224a31233846cbcb457ee923d
Woker21 <112359676+woker21@users.noreply.github.com>
2024-04-17 01:44:06 +0200

    asdasdas

--- app/components/menu.jsx
+++ app/components/menu.jsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 
 
+
 const Menu = () => {
     return (
     <>
-    <ul>
+    <ul className='flex gap-2.5 w-3/4	justify-around	mb-14	'>
+    
         <li>
             <Link href={'/'}>Home</Link>
         </li>
@@ -17,9 +19,8 @@ const Menu = () => {
         <li>
             <Link href={'/contact'}>Contact</Link>
         </li>
-        <li>
-
-        </li>
+        
+    
     </ul>
     </>
     );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,27 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Menu  from "./components/menu";
+import Menu from "./components/menu";
 
-const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Woker",
-  description: "Creado por Woker",
-};
+// const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+// export const metadata: Metadata = {
+//   title: "Woker",
+//   description: "Creado por Woker",
+// };
+
+export default function RootLayout({ children, }:
+  Readonly<{
+    children: React.ReactNode;
+  }>) {
   return (
-    <html lang="es">
-      <body className={inter.className}>
+    
 
-        <Menu/>
+      <>
         {children}
-      </body>
-    </html>
+      </>
+    
+
   );
 }
