0fa4b852325c54e1af7dce53b1bcf469e1416ede
Woker21 <112359676+woker21@users.noreply.github.com>
2024-04-17 23:11:58 +0200

    asdasd

---
+++ utils/data/contactsData.js
@@ -0,0 +1,10 @@
+export const contactsData = {
+    email: 'hernandezrichard674@gmail.com',
+    phone: '+34 647562456',
+    address: 'Avda, Meridiana 584',
+    github: 'https://github.com/woker21',
+    facebook: 'https://www.facebook.com',
+    linkedIn: 'www.linkedin.com/in/richard-hernandez21',
+    stackOverflow: 'https://stackoverflow.com',
+    devUsername: "Woker21"
+}
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Richard Hernandez",
+  profile: '/profile.png',
+  designation: "Front-end Developer",
+  description: "Front-end, digital creator with a restless mind and a keyboard ready to make magic on the web. I specialize in turning lines of code into web experiences that inspire, surprise and captivate. I love challenging convention and finding inventive solutions to complex problems. From elegant web pages to interactive applications, my approach is always to put the user experience first and bring your ideas to life with a unique and fresh touch.",
+  email: 'hernandezrichard674@gmail.com',
+  phone: '+34 647562456',
+  address: 'Avda, Meridiana 584',
+  github: 'https://github.com/said7388',
+  facebook: 'https://www.facebook.com',
+  linkedIn: 'www.linkedin.com/in/richard-hernandez21',
+  twitter: 'https://twitter.com',
+  stackOverflow: 'https://stackoverflow.com',
+  leetcode: "https://leetcode.com",
+  devUsername: "Woker21",
+  resume: "https://drive.google.com"
+}
