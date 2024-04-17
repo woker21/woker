fc870df42c9d9668d8365c666e7cc8fb4d989d6a
Woker21 <112359676+woker21@users.noreply.github.com>
2024-04-18 01:35:56 +0200

    zxcZ

--- app/layout.js
+++ app/layout.js
@@ -16,6 +16,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+          <link rel="icon" href="./favicon.ico" />
+      </head>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
